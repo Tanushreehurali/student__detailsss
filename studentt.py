e5e7c42cc5eed656135d59924b1f9d26bab1f9c0
Tanushreehurali <tanuhurali@gmail.com>
2025-11-20 15:23:22 +0530

    Update studentt.py

--- studentt.py
+++ studentt.py
@@ -4,7 +4,7 @@ if len(sys.argv) !=3:
   sys.exit(1)
   script_name = sys.argv[0]
   name=sys.argv[1]
-  roll no = sys.argv[2]
+  rollno = sys.argv[2]
   print("script name:",script_name)
   print("student name:",name)
   print("roll number:",rollno)
